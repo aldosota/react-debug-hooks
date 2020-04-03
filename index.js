915292e6c347b69a579ba8d2c9cf684185b82352
aldo <aldo.sota@fullscreendigital.ro>
2020-04-03 18:14:14 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+export default () => {
+    if(window.__REDUX_DEVTOOLS_EXTENSION__) {
+        const devTools = window.__REDUX_DEVTOOLS_EXTENSION__.connect({name: "Hooks"})
+        React.oldUseState = React.useState
+        const state = {}
+        React.useState = (initialState, debugName) => {
+            const hookResult = React.oldUseState(initialState)
+            if(debugName) {
+
+                // initial state
+                if(state[debugName] === undefined) {
+                    state[debugName] = initialState
+                    devTools.send(`${debugName}-> initial-state`, state)
+                }
+
+                // on each setState call
+                const originalSetState = hookResult[1]
+                const setState = arg => {
+                    if(typeof arg !== 'function') {
+                        state[debugName] = arg
+                        devTools.send(debugName, state)
+                        return originalSetState(arg)
+                    } else {
+                        originalSetState(s => {
+                            const result = arg(s)
+                            state[debugName] = result
+                            devTools.send(debugName, state)
+                            return result
+                        })
+                    }
+                } 
+
+                hookResult[1] = setState
+            }
+            return hookResult
+        }
+    }
+}
+
